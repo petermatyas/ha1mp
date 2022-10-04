e8f29c34e19e66794a1bc5782287698dbf2dbc02
petermatyas <mtysptr@gmail.com>
2022-10-04 15:10:12 +0200

    add map

--- geo.js
+++ geo.js
@@ -9,6 +9,15 @@ let hamRepeaters = [
 ]
 
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+console.log('---------------')
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
 
 function getLocation() {
     if (navigator.geolocation) {
